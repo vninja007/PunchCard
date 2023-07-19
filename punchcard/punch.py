2c653bcad7ab58320d158efc4964f5f6f7bf953c
FluffyCube9343 <learningninja007@gmail.com>
2023-07-19 14:26:19 -0400

    clock specific activities

--- punchcard/punch.py
+++ punchcard/punch.py
@@ -33,7 +33,7 @@ async def maketask(ctx, *, arg=""):
                 await ctx.send("Task already exists")
             else:
                 botdata[ctx.author.id]["tasks"][arg] = 0.0
-                await ctx.send("Created task "+arg)
+                await ctx.send("Created task '"+arg+"'")
                 writeData()
     else:
         await ctx.send("User does not exist! Type p.init to begin!")
@@ -51,14 +51,15 @@ async def deltask(ctx, *, arg=""):
                 await ctx.send("Task does not exist!")
             else:
                 del botdata[ctx.author.id]["tasks"][arg]
-                await ctx.send("Deleted task "+arg)
+                await ctx.send("Deleted task '"+arg+"'")
                 writeData()
     else:
         await ctx.send("User does not exist! Type p.init to begin!")
 
 
 @bot.command()
-async def start(ctx, arg=""):
+async def start(ctx, *, arg=""):
+    arg = arg.lower()
     if (ctx.author.id in botdata):
         if (botdata[ctx.author.id]["start"] == -1):
             if (arg != ""):
@@ -70,7 +71,7 @@ async def start(ctx, arg=""):
                     botdata[ctx.author.id]["start"] = getUTime()
                     botdata[ctx.author.id]["livetask"] = arg
                     writeData()
-                    await ctx.send("Clocked in at " + str(tt))
+                    await ctx.send("Clocked in for '"+arg+"' at " + str(tt))
             else:
                 ut = getUTime()
                 tt = getTime()
@@ -84,14 +85,15 @@ async def start(ctx, arg=""):
 
 
 @bot.command()
-async def stop(ctx, arg=""):
+async def stop(ctx, *, arg=""):
+    arg = arg.lower()
     if (ctx.author.id in botdata):
         if (botdata[ctx.author.id]["start"] != -1):
             ut = getUTime()
             tt = getTime()
             t1 = botdata[ctx.author.id]["start"]
             t2 = getUTime()
-            worked = round((t2-t1)/60, 2)
+            worked = float('{0:.2f}'.format((t2-t1)/60))
             hrs = (t2-t1)//3600
             mins = ((t2-t1)//60) % 60
             secs = ((t2-t1) % 60) // 1
@@ -99,11 +101,17 @@ async def stop(ctx, arg=""):
                 task = botdata[ctx.author.id]["livetask"]
                 botdata[ctx.author.id]["tasks"][task] += worked
                 botdata[ctx.author.id]["livetask"] = ""
-            botdata[ctx.author.id]["total"] += worked
-            botdata[ctx.author.id]["start"] = -1
-            botdata[ctx.author.id]["sessions"] += 1
-            writeData()
-            await ctx.send("Clocked out at " + str(tt) + ". You worked for "+str(hrs)+" hrs "+str(mins)+" mins "+str(secs)+".")
+                botdata[ctx.author.id]["total"] += worked
+                botdata[ctx.author.id]["start"] = -1
+                botdata[ctx.author.id]["sessions"] += 1
+                writeData()
+                await ctx.send("Clocked out from '"+arg+"' at " + str(tt) + ". You worked for "+str(int(hrs))+" hrs "+str(int(mins))+" mins "+str(int(secs))+" secs.")
+            else:
+                botdata[ctx.author.id]["total"] += worked
+                botdata[ctx.author.id]["start"] = -1
+                botdata[ctx.author.id]["sessions"] += 1
+                writeData()
+                await ctx.send("Clocked out at " + str(tt) + ". You worked for "+str(int(hrs))+" hrs "+str(int(mins))+" mins "+str(int(secs))+" secs.")
         else:
             await ctx.send("User is already clocked out! Use p.start to clock in!")
     else:
