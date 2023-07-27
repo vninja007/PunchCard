b69867e02eb1fab77d14407bf5430c2738bf7f49
FluffyCube9343 <learningninja007@gmail.com>
2023-07-26 22:42:48 -0400

    stable school start/end

--- punchcard/daily.py
+++ punchcard/daily.py
@@ -31,43 +31,25 @@ async def sleep(ctx, *, arg=""):
     await ctx.send("Sleep time for "+str(thedate)+" set to "+ex.split()[1])
 
 
-# @bot.command()
-# async def sleep(ctx, *, arg=""):
-#     ex = extractTime(arg)
-#     if (arg == ""):
-#         await ctx.send("Usage: p.sleep [time]")
-#     elif (ex[0:2] == "-1"):
-#         await ctx.send("Incorrect time")
-#     else:
-#         logDates(ctx.author.id, "sleep")
-#         botdata[ctx.author.id][getDate()]["sleep"] = ex
-#         writeData()
-#         await ctx.send("Sleep time for "+getDate()+" set to "+ex)
-
-
 @bot.command()
 async def schoolstart(ctx, *, arg=""):
-    ex = extractTime(arg)
-    if (arg == ""):
-        await ctx.send("Usage: p.schoolstart [time]")
-    elif (ex[0:2] == "-1"):
-        await ctx.send("Incorrect time")
-    else:
-        logDates(ctx.author.id, "wake")
-        botdata[ctx.author.id][getDate()]["schoolstart"] = ex
-        writeData()
-        await ctx.send("School start time for "+getDate()+" set to "+ex)
+    ex = getDateTime(botdata[ctx.author.id]["timezone"])
+    logDates(ctx.author.id, ex, "wake")
+    day = correctDate(ctx.author.id, ex, "wake")
+    thedate = day.split()[0]
+
+    botdata[ctx.author.id][thedate]["schoolstart"] = ex.split()[1]
+    writeData()
+    await ctx.send("School start time for "+str(thedate)+" set to "+ex.split()[1])
 
 
 @bot.command()
 async def schoolend(ctx, *, arg=""):
-    ex = extractTime(arg)
-    if (arg == ""):
-        await ctx.send("Usage: p.schoolend [time]")
-    elif (ex[0:2] == "-1"):
-        await ctx.send("Incorrect time")
-    else:
-        logDates(ctx.author.id, "sleep")
-        botdata[ctx.author.id][getDate()]["schoolend"] = ex
-        writeData()
-        await ctx.send("School end time for "+getDate()+" set to "+ex)
+    ex = getDateTime(botdata[ctx.author.id]["timezone"])
+    logDates(ctx.author.id, ex, "sleep")
+    day = correctDate(ctx.author.id, ex, "sleep")
+    thedate = day.split()[0]
+
+    botdata[ctx.author.id][thedate]["schoolend"] = ex.split()[1]
+    writeData()
+    await ctx.send("School end time for "+str(thedate)+" set to "+ex.split()[1])
